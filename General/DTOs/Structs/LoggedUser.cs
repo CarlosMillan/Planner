675f988ce3d576f3f25307c335bf436537f89f39
Carlos Millán <charlyomo@gmail.com>
2013-12-12 23:11:05 -0600

    Dto de usurio logeado

---
+++ General/DTOs/Structs/LoggedUser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace General.DTOs.Structs
+{
+    public struct LoggedUser
+    {
+        private string _userid;
+
+        #region Propierties
+        public string UserId
+        {
+            get { return _userid; }
+        }
+        #endregion
+
+        public LoggedUser(string userid)
+        {
+            _userid = userid;
+        }        
+    }
+}
