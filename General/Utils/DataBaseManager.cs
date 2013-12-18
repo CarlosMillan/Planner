78c828f83f960bfe501e9df6dabace274d6290da
Carlos Millán <charlyomo@gmail.com>
2013-12-17 21:43:50 -0600

    Primera conexion

---
+++ General/Utils/DataBaseManager.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace General.Utils
+{
+    public class DataBaseManager
+    {
+        public DataBaseManager(){ }
+
+        #region Public Methods
+
+        public DataTable GetTable(string tablename)
+        {
+            try
+            {
+                DataTable Result = new DataTable();
+                SqlConnection Connection = null;
+                OpenConnection(out Connection);
+                SqlCommand Command = new SqlCommand("SELECT * FROM USERS", Connection);                
+                SqlDataAdapter Adapter = new SqlDataAdapter(Command);
+                Adapter.Fill(Result);
+                CloseConnection(Connection);
+                return Result;
+            }
+            catch { throw; }
+        }
+
+        public void ExecuteStoredProcedure()
+        {
+            try
+            { 
+                //TODO
+            }
+            catch{ throw; }
+        }
+        #endregion
+
+        #region Private Methods
+        private void OpenConnection(out SqlConnection connectiontoopen) //probar sin parametro  OUT
+        {
+            try
+            {
+                connectiontoopen = new SqlConnection(ConfigurationManager.ConnectionStrings["CAMSA_DB"].ToString());
+                connectiontoopen.Open();
+            }
+            catch{ throw; }
+        }
+
+        private void CloseConnection(SqlConnection connectiontoclose)  //probar sin parametro REF
+        {
+            try
+            {
+                connectiontoclose.Close();                
+            }
+            catch { throw; }
+        }
+        #endregion
+    }
+}
--- Presentation/Login.aspx.cs
+++ Presentation/Login.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using General.Utils;
 
 namespace PlannerWeb
 {
@@ -12,6 +13,8 @@ namespace PlannerWeb
         protected void Page_Load(object sender, EventArgs e)
         {
             Title = "Login";
+            DataBaseManager db = new DataBaseManager();
+            db.GetTable("Users");
         }
     }
 }
